d77538433d46cdb18716f538d3e2567c1868107a
DenilsonCoutinho <denilsoncoutinho9@gmail.com>
2026-09-21 15:28:41 -0300

    Atualiza imagens dos cortes

--- src/components/FeaturedWork/project.js
+++ src/components/FeaturedWork/project.js
@@ -2,26 +2,26 @@ export const projects = [
   {
     name: "Executive Contour",
     tag: "Tesoura curta",
-    img: "/brisola-post/executive-contour-framed.png",
+    img: "/brisola-post/executive-contour-framed-v2.png",
   },
   {
     name: "Long Trim com Taper Fade",
     tag: "Tesoura média",
-    img: "/brisola-post/long-trim-taper-framed.png",
+    img: "/brisola-post/long-trim-taper-framed-v2.png",
   },
   {
     name: "Long Trim com Projeção",
     tag: "Old Money",
-    img: "/brisola-post/long-trim-projection-framed.png",
+    img: "/brisola-post/long-trim-projection-framed-v2.png",
   },
   {
     name: "Mullet",
     tag: "",
-    img: "/brisola-post/mullet-framed.png",
+    img: "/brisola-post/mullet-framed-v2.png",
   },
   {
     name: "Corte Longo",
     tag: "",
-    img: "/brisola-post/long-cut-framed.png",
+    img: "/brisola-post/long-cut-framed-v2.png",
   },
 ];
